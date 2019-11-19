742d1a723f5557cb272569c5142bf149c434a04e
m4nuelcc <manuelcarabiac@gmail.com>
2019-11-19 16:48:50 +0100

    arreglado componente de encontrar

--- src/modulos/Crud.js
+++ src/modulos/Crud.js
@@ -80,14 +80,36 @@ export default class Encontrar extends Component {
   }
 
   render() {
+    let iconodown = <i class="material-icons"> arrow_downward  </i>;
+    let iconoup =  <i class="material-icons"> arrow_upward  </i>;
+    let icono1 = this.state.ordenNombre === 1 ? iconoup : iconodown;
     return (
-      <div>
-   
-        {console.log(this.state.datos)};{JSON.stringify(this.state.datos)};
-        {/* {this.state.datosRecibidos.map( (c, i) => { 
-           return  <div key={i}> {c.datosRecibidos} </div> 
-         })} */}
-      </div>
-    );
+      <div style={{ padding: 20 }}>
+        <div className="row"> 
+          <div className="col manita"> Nombre {icono1} </div>
+          <div className="col manita"> Primer apellido {icono1} </div>
+          <div className="col manita"> Segundo apellido {icono1} </div>
+        </div>
+
+      {this.state.datos.map( (c, i) => (
+        <div key={i} className="row"> 
+          <div className="col"> {c.nombre} </div>
+          <div className="col"> {c.primerApellido} </div>
+          <div className="col"> {c.segundoApellido} </div>
+
+        </div>
+      ))}
+      </div> 
+      );
+    }
   }
-}
+  
+  // <div>
+
+  //   {/* { {console.log(this.state.datos)};{JSON.stringify(this.state.datos)};} */}
+  //   {this.state.datos.map((c, i) => {
+  //     return <div key={i}>
+  //       {c.nombre} {c.primerApellido}
+  //     </div>
+  //   })}
+  // </div>
