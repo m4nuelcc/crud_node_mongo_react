4284e0e02873ce5f10a31635952771905e5ce57e
m4nuelcc <manuelcarabiac@gmail.com>
2019-11-20 14:36:07 +0100

    nada

--- src/modulos/Crud.js
+++ src/modulos/Crud.js
@@ -149,7 +149,7 @@ export default class Encontrar extends Component {
       fetch(url, {
         method: "GET",
         headers: headers
-        // body: JSON.stringify(datosAlServer)
+
       })
         .then(res => {
           return res.json();
