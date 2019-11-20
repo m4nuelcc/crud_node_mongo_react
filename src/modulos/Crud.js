d59d3a0deb14e918fbf0a8e6e405b23eb78499bf
m4nuelcc <manuelcarabiac@gmail.com>
2019-11-20 10:43:30 +0100

    he puesto variables del state a las url para hacer el fetch en el componente listarid

--- src/modulos/Crud.js
+++ src/modulos/Crud.js
@@ -2,24 +2,23 @@ import React, { Component } from "react";
 
 export class ListarId extends Component {
   // este componente lista todos los clientes de la tabla
-  state = { datos: [] };
+  state = { datos: [],
+  db: "caja",
+  collection: "clientes", };
 
   componentDidMount() {
-    let datosAlServer = {
-      // db: "test",
-      // collection: "vino",
-      // id: "5dcaf71d775e633e9be63899"
-    };
+     
     let headers = {
       Accept: "application/json",
       "Content-Type": "application/json"
 
       // como va por GET recoge los datos en la url como por ejemplo
       // http://localhost:3000/test/vino/5dcaf71d775e633e9be63899
-      //  si pusieramos http://localhost:3000/test/vino nos sacaria toda la
+      //  si pusieramos http://localhost:3000/aborrar/naves nos sacaria toda la
       //  base de datos de vino
     };
-    let url = "http://localhost:3000/test/vino/5dcaf71d775e633e9be63899";
+    // let url = "http://localhost:3000/";
+    let url =`http://localhost:3000/${this.state.db}/${this.state.collection}`;
     fetch(url, {
       method: "GET",
       headers: headers
@@ -35,13 +34,28 @@ export class ListarId extends Component {
   }
 
   render() {
+    let iconodown = <i className="material-icons"> arrow_downward  </i>;
+    let iconoup =  <i className="material-icons"> arrow_upward  </i>;
+    let icono1 = this.state.ordenNombre === 1 ? iconoup : iconodown;
     return (
-      <div>
-        {JSON.stringify(this.state.datos)};
-        {/* {this.state.datosRecibidos.map( (c, i) => { 
-          return  <div key={i}> {c.datosRecibidos} </div> 
-        })} */}
+      <div style={{ padding: 20 }}>
+      <div className="row"> 
+        <div className="col manita"> Nombre {icono1} </div>
+        <div className="col manita"> Primer apellido {icono1} </div>
+        <div className="col manita"> Segundo apellido {icono1} </div>
+        <div className="col manita"> Telefono {icono1} </div>
       </div>
+
+    {this.state.datos.map( (c, i) => (
+      <div key={i} className="row"> 
+         <div className="col"> {c.nombre} </div>
+        <div className="col"> {c.primerApellido} </div>
+        <div className="col"> {c.segundoApellido} </div>
+        <div className="col"> {c.telefono} </div>
+
+      </div>
+    ))}
+    </div>
     );
   }
 }
@@ -89,13 +103,15 @@ export default class Encontrar extends Component {
           <div className="col manita"> Nombre {icono1} </div>
           <div className="col manita"> Primer apellido {icono1} </div>
           <div className="col manita"> Segundo apellido {icono1} </div>
+          <div className="col manita"> Telefono {icono1} </div>
         </div>
 
       {this.state.datos.map( (c, i) => (
         <div key={i} className="row"> 
-          <div className="col"> {c.nombre} </div>
+           <div className="col"> {c.nombre} </div>
           <div className="col"> {c.primerApellido} </div>
           <div className="col"> {c.segundoApellido} </div>
+          <div className="col"> {c.telefono} </div>
 
         </div>
       ))}
@@ -104,12 +120,3 @@ export default class Encontrar extends Component {
     }
   }
   
-  // <div>
-
-  //   {/* { {console.log(this.state.datos)};{JSON.stringify(this.state.datos)};} */}
-  //   {this.state.datos.map((c, i) => {
-  //     return <div key={i}>
-  //       {c.nombre} {c.primerApellido}
-  //     </div>
-  //   })}
-  // </div>
